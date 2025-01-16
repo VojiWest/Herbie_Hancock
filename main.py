20bd6a6f6c14a19ca6249c6fdbb98f9e83f7a90c
VojiWest <131270168+VojiWest@users.noreply.github.com>
2025-01-16 14:29:39 +0100

    merged Xenia into Vojo

--- main.py
+++ main.py
@@ -9,23 +9,6 @@ import numpy as np
 import sys
 import torch
 
-def train_and_run_keras_model(mode, train_X, y_one_hot, voice_ranges, class_weights, seq_length = 16): # Not used
-    if mode == "train":
-        model = keras_model.create_model(train_X[0].shape, class_weights)
-        model = keras_model.train_model(model, train_X, y_one_hot, epochs = 100)
-
-        predictions, all_preds = keras_model.predict(model, train_X, voice_ranges, seq_length = seq_length, extension_length = 500)
-
-        # save predictions to a file
-        utils.save_file(predictions, "keras_predictions", "Model Outputs/")
-        utils.save_file(all_preds, "keras_all_preds", "Model Outputs/")
-
-    elif mode == "eval":
-        predictions = np.load("Model Outputs/keras_predictions.npy")
-        all_preds = np.load("Model Outputs/keras_all_preds.npy")
-
-    return predictions, all_preds
-
 def train_and_run_pytorch_model(mode, train_X, y_one_hot, voice_ranges, class_weights, seq_length = 16):
     if mode == "train":
         # Convert things to tensors since PyTorch works with tensors
@@ -66,56 +49,52 @@ def train_and_run_pytorch_model(mode, train_X, y_one_hot, voice_ranges, class_we
 
     return predictions, all_preds
 
-def main(mode):
+def main():
     data = preprocess.load_data(path = "F.txt") # Load the data
     data = data[0] # we choose only voice 1 
-
-    ### Plotting the  
-    # plot.plot_data(data, title = "Original Data", xlabel = "Time", ylabel = "Note")
-    # plot.plot_histogram(data, title = "Histogram of Original Data", xlabel = "Note", ylabel = "Count")
-
-    # data_to_audio(data, "original")
 
     """ Preprocess data """
 
     train, val, test = preprocess.temporal_data_split(data)
-    train_X, train_Y = preprocess.create_input_windows(train, seq_length = 50)
+    # train_X_window, train_Y = preprocess.create_input_windows(train, seq_length = 50)
 
     voice_ranges = [(50, 80), (40, 70), (35, 65), (25, 55)]
     voice_range_voice1 = (50, 80)
-    y_one_hot = preprocess.one_voice_convert_onehot(train_Y, voice_range_voice1)
+    # y_one_hot = preprocess.one_voice_convert_onehot(train_Y, voice_range_voice1)
 
-
+    augmentated_data = augmentation.augmented_encoding(train)
 
     circle_of_fifths_representation = augmentation.convert_voices_circle_of_fifths(train)
-    print(circle_of_fifths_representation[353])
+    print("Circle of 5ths Representation Example: ", circle_of_fifths_representation[350])
+    chromatic_circle_representation = augmentation.convert_voices_chromatic_circle(train)
+    print("Chromatic circle Representation Example: ", chromatic_circle_representation[350])
 
+    augmentation.plot_circle(circle_of_fifths_representation)
+    augmentation.plot_circle(chromatic_circle_representation)
 
-
-    augmentation.plot_circle_of_fifths(circle_of_fifths_representation[353])
 
 
     # plot the one hot data
     # plot.plot_one_hot_labels(y_one_hot, title = "One Hot Labels", xlabel = "Time", ylabel = "Note")
 
-    class_weights = preprocess.get_class_weights(y_one_hot)
+    # class_weights = preprocess.get_class_weights(y_one_hot)
 
 
     # plot.plot_class_weights(utils.weights_to_tensor(class_weights))
 
-    """ Train and run model """
+    # """ Train and run model """
 
-    predictions, all_preds = train_and_run_pytorch_model(mode, train_X, y_one_hot, voice_ranges, class_weights, seq_length = 50)
+    # predictions, all_preds = train_and_run_pytorch_model(mode, train_X, y_one_hot, voice_ranges, class_weights, seq_length = 50)
 
-    """ Postprocess data """
+    # """ Postprocess data """
 
-    new_data, new_predictions = utils.add_preds_to_data(train, predictions)
+    # new_data, new_predictions = utils.add_preds_to_data(train, predictions)
 
-    plot.plot_certainty(all_preds, title = "Certainty of Predictions", xlabel = "Time", ylabel = "Note")
-    plot.plot_data(new_data, title = "Original + Predicted Data", xlabel = "Time", ylabel = "Note")
-    plot.plot_data(new_predictions, title = "Predicted Data", xlabel = "Time", ylabel = "Note")
+    # plot.plot_certainty(all_preds, title = "Certainty of Predictions", xlabel = "Time", ylabel = "Note")
+    # plot.plot_data(new_data, title = "Original + Predicted Data", xlabel = "Time", ylabel = "Note")
+    # plot.plot_data(new_predictions, title = "Predicted Data", xlabel = "Time", ylabel = "Note")
 
-    audio_midi.data_to_audio(new_data, "og + pred")
+    # audio_midi.data_to_audio(new_data, "og + pred")
 
 
     
